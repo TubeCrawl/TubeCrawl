5d1938550029980bbaa3175275278731ea905b6b
PatrickAberle <m-strive@gmx.de>
2018-05-04 12:53:08 +0200

    modified test

--- test.py
+++ test.py
@@ -1,16 +1,2 @@
-from bs4 import BeautifulSoup
-import requests
-
-# Get an example page
-page = requests.get("https://dataquestio.github.io/web-scraping-pages/simple.html")
-print(page.content)
-print()
-print()
-print("===================================================================" )
-print()
-print()
-
-# Parse the page with beautifulsoup, print it out nicely
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
+print("Hello")
 
